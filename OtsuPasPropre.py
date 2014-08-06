0715bf721f7fbf8b7e16002e9d0ec5df9115d074
aliciamale <Alicia@MacBook-Air-Alicia.local>
2014-08-06 14:36:22 +0200

    ajout OtsuPasPropre.py

---
+++ OtsuPasPropre.py
@@ -0,0 +1,37 @@
+#!/user/amale/home/python2.7
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 14 17:25:20 2014
+
+@author: amale
+"""
+
+import scipy.misc as smisc
+
+import mahotas
+
+
+path = '/user/amale/home/Pictures/MC70/'
+name = 'IMG_05'
+ext = '.JPG'
+name2 = 'Otsu/IMG_Otsu_05'
+
+for i in range(28,67) :
+    
+    ##### Reading Image #####
+    img = mahotas.imread(path+name+str(i)+ext)
+    
+#    print img.shape
+#    print img.dtype
+#    print img.max()
+#    print img.min()
+    
+    ##### Segmentation #####
+    T = mahotas.thresholding.otsu(img)
+
+    ##### Saving Image Segmented #####    
+    A = img > T
+    smisc.imsave(path+name2+str(i)+ext,A)
+    
+#pl.show()
